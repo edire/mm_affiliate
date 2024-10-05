8e0b032e616c9f0149458f9a9651b5cf03df1e12
edire <eric.di.re@gmail.com>
2024-10-05 09:11:34 -0700

    added multi-url to env_var

--- models/affiliate_system/stage/snowplow_events/stg_events__affiliate_hits.sql
+++ models/affiliate_system/stage/snowplow_events/stg_events__affiliate_hits.sql
@@ -3,7 +3,17 @@
     materialized = 'table'
 )}}
 
-WITH affiliate_hits as (
+WITH url_host_env AS (
+  SELECT '{{ env_var("URL_HOST") }}' AS url_host_str
+)
+
+, url_host_tbl as (
+  SELECT TRIM(url_host_val) AS url_host
+  FROM url_host_env,
+    UNNEST(SPLIT(url_host_str, ',')) AS url_host_val
+)
+
+, affiliate_hits as (
   SELECT 
     '{{ env_var("OFFER") }}' as app_id
     , domain_userid 
@@ -18,10 +28,10 @@ WITH affiliate_hits as (
     , geo_region	
     , geo_city
 
-  FROM {{ source('raw_events', 'events') }} 
+  FROM {{ source('raw_events', 'events') }} e
   WHERE DATE(collector_tstamp, 'US/Arizona') >= '{{ env_var("START_DATE") }}'
     and event = 'unstruct'
-    and (page_urlhost in ('{{ env_var("URL_HOST") }}')
+    and (page_urlhost in (select url_host from url_host_tbl)
       or lower(app_id) in ('{{ env_var("OFFER") }}'))
     AND unstruct_event_com_deangraziosi_affiliate_tracking_2_1_0_2.affiliate_id IS NOT NULL
     AND unstruct_event_com_deangraziosi_affiliate_tracking_2_1_0_2.affiliate_id not in ('0','100000')
